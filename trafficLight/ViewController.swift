383494b8fe69cf09baebc07138920c85d0811abe
Сергей Макаров <ronaldo096@bk.ru>
2025-03-24 23:41:23 +0300

    add button, redLightView, yellowLightView, greenLightView

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TrafficLightColor{
+    case red, yellow, green
+}
 
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var changeStateButton: UIButton!
+    
+    private var trafficLightColor = TrafficLightColor.red
+    private let onLight = 1.0
+    private let offLight = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = offLight
+        yellowLightView.alpha = offLight
+        greenLightView.alpha = offLight
+        
+        changeStateButton.layer.cornerRadius = 10
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
     }
-
-
+    
+    @IBAction func changeStateButtonPressed(_ sender: UIButton) {
+        changeStateButton.setTitle("NEXT", for: .normal )
+                
+        
+                switch trafficLightColor {
+                case .red:
+                    redLightView.alpha = onLight
+                    greenLightView.alpha = offLight
+                    trafficLightColor = .yellow
+                case .yellow:
+                    redLightView.alpha = offLight
+                    yellowLightView.alpha = onLight
+                    trafficLightColor = .green
+                case .green:
+                    yellowLightView.alpha = offLight
+                    greenLightView.alpha = onLight
+                    trafficLightColor = .red
+                }
+    }
+    
 }
 
